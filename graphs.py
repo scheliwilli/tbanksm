8d0aabb3018f60fdcc525b3d89065414dfcfa7df
User <nikitka.mikheev.2000@bk.ru>
2026-04-03 15:12:24 +0700

    added get min changes 2

--- graphs.py
+++ graphs.py
@@ -3,24 +3,27 @@ from enum import Enum
 from collections import deque
 import json
 
-class TransportType (Enum):
+
+class TransportType(Enum):
     TRAIN = 1
     PLANE = 2
     ELECTRICTRAIN = 3
 
-class parametres (Enum):
+
+class parametres(Enum):
     COST = 1
     CHANGE = 2
 
+
 class Flight:
-    def __init__ (self,
-                  cityA: str,
-                  cityB : str, 
-                  start_time : datetime, 
-                  arrive_time : datetime, 
-                  cost : int,
-                  id: int,
-                  transport_type: TransportType):
+    def __init__(self,
+                 cityA: str,
+                 cityB: str,
+                 start_time: datetime,
+                 arrive_time: datetime,
+                 cost: int,
+                 id: int,
+                 transport_type: TransportType):
         self.cityA = cityA
         self.cityB = cityB
         self.start_time = start_time
@@ -30,16 +33,15 @@ class Flight:
         self.id = id
         self.transport_type = transport_type
 
-    def  __str__(self):
-        return str(self.cityA) +\
-                str(self.cityB) + \
-                str(self.start_time) + \
-                str(self.arrive_time) + \
-                str(self.cost) + \
-                str(self.duration) + \
-                str(self.id) + \
-                str(self.transport_type)
-
+    def __str__(self):
+        return str(self.cityA) + \
+            str(self.cityB) + \
+            str(self.start_time) + \
+            str(self.arrive_time) + \
+            str(self.cost) + \
+            str(self.duration) + \
+            str(self.id) + \
+            str(self.transport_type)
 
 
 class Graph:
@@ -51,7 +53,7 @@ class Graph:
         with open("flight.json", "r", encoding="utf-8") as f:
             # Загружаем данные из файла в переменную
             data = json.load(f)
-        
+
         #   Сохраняем json  в граф
         for name, flight_list in data.items():
             self.graph[name] = []
@@ -67,10 +69,10 @@ class Graph:
                 )
                 self.graph[name].append(flight)
 
-    def get_min_changes (self, cityA:str, cityB:str, start_time):
+    def get_min_changes(self, cityA: str, cityB: str, start_time):
         changes = {name: float("inf") for name, flights in self.graph.items}
         time = {name: float("inf") for name, flights in self.graph.items}
-        previos_flight = {name : -1 for name, flights in self.graph.items()}
+        previos_flight = {name: -1 for name, flights in self.graph.items()}
         q = deque()
 
         time[cityA] = start_time
@@ -79,7 +81,7 @@ class Graph:
         while (q):
             curr_city = q.popleft()
             for flight in self.graph[cityA]:
-                if flight.start_time < time[curr_city] + self.flight_delay:        # Проверка успеем ли на рейс
+                if flight.start_time < time[curr_city] + self.flight_delay:  # Проверка успеем ли на рейс
                     continue
 
                 if (time[flight.cityB] > flight.end_time and changes[curr_city.id] + 1 == changes[flight.cityB.id]):
@@ -92,7 +94,6 @@ class Graph:
                     previos_flight[flight.cityB] = flight
                     q.append(flight.cityB)
 
-
         flight_lst = []
         current_city = cityB
         while previos_flight[current_city] != -1:
@@ -101,8 +102,7 @@ class Graph:
 
         return changes[cityB]
 
-
-    def get_min_cost(self, cityA:str, cityB:str, start_time):
+    def get_min_cost(self, cityA: str, cityB: str, start_time):
         changes = {name: float("inf") for name, flights in self.graph.items}
         time = {name: float("inf") for name, flights in self.graph.items}
         q = deque()
@@ -121,6 +121,7 @@ class Graph:
                     if (time[flight.cityB] > flight.end_time):
                         time[flight.cityB.id] = flight.end_time
                     q.append(flight.cityB)
+
 
 test = Graph()
 graph = test.graph
